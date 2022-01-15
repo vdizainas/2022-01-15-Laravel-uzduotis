7f93e84c402653bf3ab1f6f65c30a299f84f974f
vdizainas <80696811+vdizainas@users.noreply.github.com>
2022-01-15 11:14:33 +0200

    sukurta modeliai ir keliai

--- projektas/routes/web.php
+++ projektas/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('clients')->group(function() {
+    Route::get('', 'App\Http\Controllers\ClientController@index')->name('clients.index');
+    Route::get('create', 'App\Http\Controllers\ClientController@create')->name('clients.create');
+    Route::get('edit', 'App\Http\Controllers\ClientController@edit')->name('clients.edit');
+});
+
+Route::prefix('companies')->group(function() {
+    Route::get('', 'App\Http\Controllers\CompanyController@index')->name('companies.index');
+    Route::get('create', 'App\Http\Controllers\CompanyController@create')->name('companies.create');
+    Route::get('edit', 'App\Http\Controllers\CompanyController@edit')->name('companies.edit');
+});
